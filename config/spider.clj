1ee3ba562e5601fcb8a7b7eb219c25f78221e2fe
Aleksander Madland Stapnes <madland.stapnes@gmail.com>
2017-02-04 19:07:26 -0300

    Some different ways to make interconnected components

--- config/spider.clj
+++ config/spider.clj
@@ -15,22 +15,25 @@
 
 (http/handler :spider/echo 'spider.core/echo-handler)
 
+;; The return value of http/handler, and presumably other component generating
+;; dsl functions, is an eid which can be used instead of specifying a specific keyword
+#_(def greet-eid (http/handler 'spider.core/greet-handler))
+
 
 (pedestal/server :spider/server 8000
 
   (http/endpoint :get "/" :spider/hello)
 
-  (http/endpoint :get "/greet/" :spider/greet)
-
   (http/endpoint :get "/greet/:name" :spider/greet)
 
   (http/endpoint :get "/echo/:x" :spider/echo)
 
-  ;; A handler can also be specified inline
+  ;; Using the returned eid instead of an arachne id.
+  #_(http/endpoint :get "/greet/:name" greet-eid)
+
+  ;; A handler can also be specified inline, with or without an id
   #_(http/endpoint :get "/greet/:name" (http/handler :spider/greet 'spider.core/greet-handler))
 
-  ;; But this doesn't work for some reason.
   #_(http/endpoint :get "/greet/:name" (http/handler 'spider.core/greet-handler))
-
 
   )
